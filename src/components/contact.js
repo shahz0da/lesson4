e082cd304054ee8c38fe3ceb9405fe169d175e2f
shahz0da <shahzodabeshimova33@gmail.com>
2022-08-06 11:48:23 +0500

    contact added

---
+++ src/components/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic et quidem distinctio vitae voluptates porro sunt iste consequuntur esse, odio voluptatum laudantium autem sapiente delectus fuga assumenda deserunt exercitationem numquam!</p>
+    </div>
+  )
+}
+
+export default contact
