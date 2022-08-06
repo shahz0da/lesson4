8d7c02495a21f855d15db37a83b52a1675e7c751
shahz0da <shahzodabeshimova33@gmail.com>
2022-08-06 11:59:22 +0500

    asdf

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const contact = () => {
+const Contact = () => {
   return (
     <div>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic et quidem distinctio vitae voluptates porro sunt iste consequuntur esse, odio voluptatum laudantium autem sapiente delectus fuga assumenda deserunt exercitationem numquam!</p>
